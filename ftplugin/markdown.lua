4680cd445645e6c49d8436a346dd39e2e614bee9
jghauser <julian@julianhauser.com>
2021-06-12 21:07:33 +0200

    Fix keymap

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,7 +2,7 @@
 -- FOLLOW MD LINKS
 --
 
-local nvim_buf_set_keymap = vim.api.nvim_set_keymap
+local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
 
 -- follow md links
-nvim_buf_set_keymap('n', '<cr>', ':lua require("follow-md-links").follow_link()<cr>', {noremap = true, silent = true})
+nvim_buf_set_keymap('0', 'n', '<cr>', ':lua require("follow-md-links").follow_link()<cr>', {noremap = true, silent = true})
